b88e497b18ca8434396a6d92cacb2b65a5a67eb0
tahaamin2013 <tahaamindob2013@gmail.com>
2024-08-29 16:50:16 +0500

    hhhhhh

--- src/app/googletagmanager/button.tsx
+++ src/app/googletagmanager/button.tsx
@@ -1,14 +1,21 @@
 import React, { ForwardRefExoticComponent, RefAttributes } from 'react';
 
+// Define the ButtonProps interface with explicit types
 interface ButtonProps {
   onClick: () => void;
-  label: string;  // Using 'label' instead of 'children'
+  label: string;
 }
 
-const MyButton: ForwardRefExoticComponent<ButtonProps & RefAttributes<HTMLButtonElement>> = React.forwardRef(({ onClick, label }, ref) => (
-  <button onClick={onClick} ref={ref}>
-    {label}
-  </button>
-));
+// Define the Button component with React.forwardRef
+const MyButton: ForwardRefExoticComponent<ButtonProps & RefAttributes<HTMLButtonElement>> = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ onClick, label }: any, ref:any) => (
+    <button onClick={onClick} ref={ref}>
+      {label}
+    </button>
+  )
+);
+
+// Set the display name for the component
+MyButton.displayName = 'MyButton';
 
 export { MyButton };
