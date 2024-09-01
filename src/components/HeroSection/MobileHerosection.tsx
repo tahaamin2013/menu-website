dec7e3af0234532cee8b71bc50f0f9702db65cae
tahaamin2013 <tahaamindob2013@gmail.com>
2024-09-01 10:21:54 +0500

    fd

--- src/components/HeroSection/MobileHerosection.tsx
+++ src/components/HeroSection/MobileHerosection.tsx
@@ -56,45 +56,9 @@ const MobileHerosection = () => {
 
   return (
     <>
-      {loading ? (
-        <div className=" pb-3 items-center px-7 border-b mb-3 block md:hidden">
-          <Skeleton className="mb-2 h-[400px] md:mt-2 w-full rounded-xl" />
-        </div>
-      ) : (
-        <Carousel className="h-[330px] mb-6 lg:hidden block overflow-hidden">
-          <div className="w-[300px] absolute -left-[13rem] -bottom-0 h-[200px] bg-orange-300 rounded-full blur-3xl" />
-          <div className="w-[400px] absolute -right-[13rem] top-[10px] h-[307px] bg-[#C0E8A6] rounded-full blur-2xl" />
-          <CarouselContent>
-            {allProducts.map((product, index) => {
-              const link = convertNameToLink(product.name);
-              console.log(link);
-              return (
-                <CarouselItem
-                  key={product.link}
-                  className="flex flex-col gap-3 items-center px-10 justify-center text-center"
-                >
-                  <Image
-                    className="rounded-full"
-                    src={product.image}
-                    alt={`Starbucks Product`}
-                    width={160}
-                    height={100}
-                    loading="lazy"
-                  />
-                  {/* TODO: chage above image */}
-                  <span className="font-bold text-xl">{product.name}</span>
-                  <Link
-                    href={`/${link}`}
-                    className="w-fit px-6 text-white rounded-full bg-primary py-2 text-[15px] duration-500 transition-all"
-                  >
-                    View Price & Calories
-                  </Link>
-                </CarouselItem>
-              );
-            })}
-          </CarouselContent>
-        </Carousel>
-      )}
+   page
+     
+      
     </>
   );
 };
