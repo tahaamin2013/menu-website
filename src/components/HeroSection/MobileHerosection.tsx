2c857bf4b7403e4d138adb9d4c47f9661ae5d4ee
tahaamin2013 <tahaamindob2013@gmail.com>
2024-09-01 19:55:29 +0500

    fd

--- src/components/HeroSection/MobileHerosection.tsx
+++ src/components/HeroSection/MobileHerosection.tsx
@@ -28,8 +28,6 @@ function convertNameToLink(name: any) {
 // Fisher-Yates shuffle algorithm
 function shuffleArray(array: any[]) {
   for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
   }
   return array;
 }
